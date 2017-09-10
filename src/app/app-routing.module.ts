748ce64c9d185f9e146244741ce0f58347300b25
Gaspar Habif <gasparhabif@gmail.com>
2017-09-10 20:27:45 -0300

    First Upload. Only file creations

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddressCheckComponent }   from './components/address-check/address-check.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/addresscheck', pathMatch: 'full' },
+  { path: 'addresscheck', component: AddressCheckComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
