a30bc1aaac9d4d9a701df4e3c38225ff92495b6d
Gaspar Habif <gasparhabif@gmail.com>
2017-09-14 11:47:54 -0300

    First edits

---
+++ src/app/components/address-check/address-check.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'address-check',
+  templateUrl: './address-check.component.html',
+  styleUrls: ['./address-check.component.scss']
+})
+
+export class AddressCheckComponent{
+
+
+}
